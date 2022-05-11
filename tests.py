afacf1c35c5f2d22e8d4f5f85a1f7b2440bbaccd
Christopher J. Markiewicz <markiewicz@stanford.edu>
2022-05-11 13:17:54 -0400

    TEST: Adapt original cpython tests

--- tests.py
+++ tests.py
@@ -35,3 +35,24 @@ def test_LooseVersion_compat(v1, v2):
     assert (orig1 > vend2) == (v1 > v2)
     assert (orig1 <= vend2) == (v1 <= v2)
     assert (orig1 >= vend2) == (v1 >= v2)
+
+
+# Adapted from Cpython:Lib/distutils/tests/test_version.py
+@pytest.mark.parametrize("v1,v2,result",
+    [('1.5.1', '1.5.2b2', -1),
+     ('161', '3.10a', 1),
+     ('8.02', '8.02', 0),
+     ('3.4j', '1996.07.12', -1),
+     ('3.2.pl0', '3.1.1.6', 1),
+     ('2g6', '11g', -1),
+     ('0.960923', '2.2beta29', -1),
+     ('1.13++', '5.5.kw', -1)])
+def test_cmp(v1, v2, result):
+    loosev1 = lv.LooseVersion(v1)
+    loosev2 = lv.LooseVersion(v2)
+    assert loosev1._cmp(loosev2) == result
+    assert loosev1._cmp(v2) == result
+    assert loosev2._cmp(loosev1) == -result
+    assert loosev2._cmp(v1) == -result
+    assert loosev1._cmp(object()) == NotImplemented
+    assert loosev2._cmp(object()) == NotImplemented
